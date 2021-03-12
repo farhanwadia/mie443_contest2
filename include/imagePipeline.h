ef87e5a7be5441cf4684c1d813346c55601c0ec4
henrycueva <62361706+henrycueva@users.noreply.github.com>
2021-03-12 13:05:02 -0500

    Update imagePipeline.h

--- include/imagePipeline.h
+++ include/imagePipeline.h
@@ -7,6 +7,14 @@
 #include <cv_bridge/cv_bridge.h>
 #include <boxes.h>
 
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/xfeatures2d.hpp
+
 using namespace cv;
 using namespace cv::xfeatures2d;
 
